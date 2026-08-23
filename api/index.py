02d4a868db6f4ae925e5bb46da27d6370639addb
mathew-emad <mathyouemad@gmail.com>
2026-08-23 15:00:55 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import sys
+import os
+
+# إضافة المجلد الرئيسي للـ path عشان يقدر يقرأ من مجلد backend
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from backend.app_moment import app
